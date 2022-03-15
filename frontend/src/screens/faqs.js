5daf06e0a722663ffd1bdc5a786fafa5813c013d
Vidhi Davda <vidhidavda1121@gmail.com>
2022-03-16 00:46:25 +0530

    first commit

--- frontend/src/screens/faqs.js
+++ frontend/src/screens/faqs.js
@@ -1,21 +1,18 @@
 import React from 'react'
 
 
-
 const FAQS = () => {
-    const manSubmitFormReducer=useSelector(state => state.manSubmitFormReducer)
-    const {loading,error,men} = manSubmitFormReducer
   return (
     <div>
-    <h1>
+    <h4>
     1. How much salary can I expect?
-    </h1>
+    </h4>
     <p>
     Ans. You can expect a minimum salary of 25,000 INR and can go up to 45,000 INR.
  The salary offered will depend on your skills, experience and performance in the interview.
     </p>
     
-    <h1>2. What is the eligibility criteria to apply for this job?</h1>
+    <h4>2. What is the eligibility criteria to apply for this job?</h4>
     <p>
 Ans. The candidate should have completed 10th or Below 10th degree and people
  who are freshers are eligible to apply for this job. 
